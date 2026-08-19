47b5fb6e238c4bed3ff2986ee19587577ad02a21
johnjesrin2007-cmyk <johnjesrin2007@gmail.com>
2026-08-19 12:55:42 +0530

    remove zero's

--- practice.py
+++ practice.py
@@ -1,18 +1,19 @@
-numbers = [2, 5, 1, 8, 2, 9, 3]
-k = 3
+numbers = [0, 1, 0, 3, 12]
+#numbers=[1,0,0,3,12]
+#numbers=[1,0,0,3,12]
+#numbers=[1,3,0,0,12]
+#numbers=[1,3,12,0,0]
 
-window_sum = 0
+left=0
+right=1
 
-for i in range(k):
-    window_sum += numbers[i]
+while right<len(numbers):
+    if numbers[left]==0 and numbers[right]!=0:
+        numbers[left], numbers[right] = numbers[right], numbers[left]
+        left+=1
+        right+=1
+    elif numbers[left]==numbers[right]==0:
+        right+=1
 
-largest = window_sum
+print(numbers)
 
-for i in range(k, len(numbers)):
-    window_sum += numbers[i]
-    window_sum -= numbers[i - k]
-
-    if window_sum > largest:
-        largest = window_sum
-
-print("Maximum Sum:", largest)
