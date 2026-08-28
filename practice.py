dc981166d57eef66da4925052274ed001116aba0
johnjesrin2007-cmyk <johnjesrin2007@gmail.com>
2026-08-28 11:31:32 +0530

    Binary search variation

--- practice.py
+++ practice.py
@@ -1,6 +1,6 @@
 numbers = [2, 5, 8, 12, 18, 21, 27, 31, 40]
-target = 18
-
+target = 20
+found=False
 left=0
 right=len(numbers)-1
 
@@ -8,9 +8,11 @@ while left<=right:
     mid= (left+right)//2
 
     if numbers[mid]==target:
-        print(mid)
-        break
+       found=True
+       break
     elif numbers[mid]<target:
         left=mid+1
     else:
         right=mid-1
+
+print(found)
