fd634265baf77e9d00bc333057bce4df1181e1d3
johnjesrin2007-cmyk <johnjesrin2007@gmail.com>
2026-08-28 11:43:10 +0530

    NOT FOUND CONCEPT

--- practice.py
+++ practice.py
@@ -1,18 +1,21 @@
-numbers = [2, 5, 8, 12, 18, 21, 27, 31, 40]
+numbers = [2, 5, 8, 12, 18, 21, 27]
 target = 20
-found=False
-left=0
-right=len(numbers)-1
 
-while left<=right:
-    mid= (left+right)//2
+left = 0
+right = len(numbers) - 1
 
-    if numbers[mid]==target:
-       found=True
-       break
-    elif numbers[mid]<target:
-        left=mid+1
+while left <= right:
+    mid = (left + right) // 2
+
+    if numbers[mid] == target:
+        print(mid)
+        break
+
+    elif numbers[mid] < target:
+        left = mid + 1
+
     else:
-        right=mid-1
+        right = mid - 1
 
-print(found)
+else:
+    print(left)
