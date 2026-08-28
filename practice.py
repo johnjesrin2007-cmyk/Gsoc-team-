c4035376d76270fc15366d74e81440343cac6f22
johnjesrin2007-cmyk <johnjesrin2007@gmail.com>
2026-08-28 13:08:42 +0530

    count occurances using binary search

--- practice.py
+++ practice.py
@@ -1,21 +1,40 @@
-numbers = [2, 5, 8, 12, 18, 21, 27]
-target = 20
+numbers = [1, 4, 6, 6, 6, 6, 9, 12, 15]
+target = 6
 
 left = 0
 right = len(numbers) - 1
+first_occurance=-1
 
 while left <= right:
     mid = (left + right) // 2
 
     if numbers[mid] == target:
-        print(mid)
-        break
-
+       first_occurance=mid
+       right=mid-1
     elif numbers[mid] < target:
         left = mid + 1
 
     else:
         right = mid - 1
 
-else:
-    print(left)
+
+
+left=0
+right=len(numbers)-1
+last_occurance=-1
+
+while left<=right:
+    mid=(left+right)//2
+
+    if numbers[mid]==target:
+        last_occurance=mid
+        left=mid+1
+    elif numbers[mid]<target:
+        left=mid+1
+    else:
+        right=mid-1
+
+count=last_occurance-first_occurance+1
+print(f"How many times {target} appears is: {count}")
+
+
