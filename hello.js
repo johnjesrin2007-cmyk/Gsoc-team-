0f1d845e8b3786565c8d56ff654621b6d728e77a
johnjesrin2007-cmyk <johnjesrin2007@gmail.com>
2026-09-07 11:50:44 +0530

    filter

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 const numbers = [10, 20, 30, 40, 50]
 
-const multiplied = numbers.map((number)=>{
-    return number*3
+const multiplied = numbers.filter((number)=>{
+    return number > 20
 })
 
 console.log(multiplied)
